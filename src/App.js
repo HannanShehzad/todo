6acc88c9af3883d73eeb6f7b0d1851102f90d0f0
Hannan <hannan.shehzad201999@gmail.com>
2023-08-19 01:04:53 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title: item, completed: false },
+    ]);
+
+    setItem(""); // Clear the input field after submitting
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            type="text"
+            id="item"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">todo list</h1>
+      <ul className="list">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                name=""
+                id=""
+              />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
